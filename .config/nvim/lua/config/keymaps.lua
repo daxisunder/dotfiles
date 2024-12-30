83e7f522dfc8207171d5b53473de8f4e63caed7a
daxis <no-prophet@proton.me>
2024-12-30 04:16:16 +0100

    remove jk

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,11 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Set jk to ESC in insert mode
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
--- Set jk to ESC in normal mode
-vim.api.nvim_set_keymap("n", "jk", "<Esc>", { noremap = true })
-
 -- Floaterm
 vim.api.nvim_set_keymap("n", "<F7>", ":FloatermNew<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<F8>", ":FloatermKill<CR>", { noremap = true })
