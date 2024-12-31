bcf4f4abcd77e2da57e5872f600a8cc2f6736126
daxis <no-prophet@proton.me>
2024-12-31 05:51:34 +0100

    add yaziline.yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,7 +1,8 @@
 return {
-	"plugins/full-borde",
+	"plugin/full-border",
 	require("full-border"):setup({
 		-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 		type = ui.Border.ROUNDED,
 	}),
+	require("yaziline"):setup(),
 }
