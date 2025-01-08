5d5618811d60ce2a56bd247e3ed9d5a476419c15
daxis <no-prophet@proton.me>
2025-01-08 10:02:04 +0100

    set snippets source to default
    if set to any other issues arise, also remove LuaSnip

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -1,25 +1,75 @@
 return {
   "saghen/blink.cmp",
-  config = function()
-    require("blink.cmp").setup({
-      sources = {
-        completion = {
-          enabled_providers = { "lsp", "path", "snippets", "buffer", "orgmode" },
-          -- Or if you want to use only this provider in org files
-          -- enabled_providers = function()
-          --   if vim.bo.filetype == 'org' then
-          --     return { 'orgmode' }
-          --   end
-          --   return { 'lsp', 'path', 'snippets', 'buffer' }
-          -- end
-        },
-        providers = {
-          orgmode = {
-            name = "Orgmode",
-            module = "orgmode.org.autocompletion.blink",
-          },
-        },
+  dependencies = {
+    { "echasnovski/mini.snippets" },
+    { "rafamadriz/friendly-snippets" },
+    { "giuxtaposition/blink-cmp-copilot" },
+  },
+  opts = {
+    snippets = { preset = "default" },
+    completion = {
+      menu = { border = "rounded" },
+      documentation = { window = { border = "rounded" } },
+      keyword = { range = "full" },
+      accept = { auto_brackets = { enabled = false } },
+      list = { selection = { preselect = false, auto_insert = true } },
+    },
+    signature = { window = { border = "rounded" } },
+  },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer", "copilot" },
+    providers = {
+      copilot = {
+        name = "copilot",
+        module = "blink-cmp-copilot",
+        score_offset = 100,
+        async = true,
+        transform_items = function(_, items)
+          local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+          local kind_idx = #CompletionItemKind + 1
+          CompletionItemKind[kind_idx] = "Copilot"
+          for _, item in ipairs(items) do
+            item.kind = kind_idx
+          end
+          return items
+        end,
       },
-    })
-  end,
+    },
+  },
+  appearance = {
+    -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+    kind_icons = {
+      Copilot = "",
+      Text = "󰉿",
+      Method = "󰊕",
+      Function = "󰊕",
+      Constructor = "󰒓",
+
+      Field = "󰜢",
+      Variable = "󰆦",
+      Property = "󰖷",
+
+      Class = "󱡠",
+      Interface = "󱡠",
+      Struct = "󱡠",
+      Module = "󰅩",
+
+      Unit = "󰪚",
+      Value = "󰦨",
+      Enum = "󰦨",
+      EnumMember = "󰦨",
+
+      Keyword = "󰻾",
+      Constant = "󰏿",
+
+      Snippet = "󱄽",
+      Color = "󰏘",
+      File = "󰈔",
+      Reference = "󰬲",
+      Folder = "󰉋",
+      Event = "󱐋",
+      Operator = "󰪚",
+      TypeParameter = "󰬛",
+    },
+  },
 }
