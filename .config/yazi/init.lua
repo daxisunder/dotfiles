cf8ba19d18018dec3f4b79731826a3efbad4c44f
daxis <no-prophet@proton.me>
2025-02-02 00:23:40 +0100

    add plugins

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -5,4 +5,25 @@ return {
 		type = ui.Border.ROUNDED,
 	}),
 	require("yaziline"):setup(),
+	require("githead"):setup(),
+	require("git"):setup(),
+	require("restore"):setup({
+		-- Set the position for confirm and overwrite dialogs.
+		-- don't forget to set height: `h = xx`
+		-- https://yazi-rs.github.io/docs/plugins/utils/#ya.input
+		position = { "center", w = 70, h = 40 },
+
+		-- Show confirm dialog before restore.
+		-- NOTE: even if set this to false, overwrite dialog still pop up
+		show_confirm = true,
+
+		-- colors for confirm and overwrite dialogs
+		theme = {
+			title = "blue",
+			header = "green",
+			-- header color for overwrite dialog
+			header_warning = "yellow",
+			list_item = { odd = "blue", even = "blue" },
+		},
+	}),
 }
