60a12e5b057c507505a254ef9e9f7b34effd637a
daxis <no-prophet@proton.me>
2025-02-08 09:36:18 +0100

    set border (still no worky)

--- .config/nvim/lua/plugins/git-messenger.lua
+++ .config/nvim/lua/plugins/git-messenger.lua
@@ -7,13 +7,13 @@ return {
     vim.g.git_messenger_always_into_popup = 1
     vim.g.git_messenger_popup_max_width = 80
     vim.g.git_messenger_popup_executable = "fzf"
-    vim.g.git_messenger_popup_content_margin = "true"
+    vim.g.git_messenger_popup_content_margins = "true"
     vim.g.git_messenger_floating_win_opts = {
       border = "rounded",
-      focusable = false,
-      enter = true,
-      highlight = "Normal:Normal",
+      highlights = {
+        border = "FloatBorder",
+      },
     }
-    vim.api.nvim_set_keymap("n", "<leader>gm", ":GitMessenger<CR>", { noremap = true, silent = true })
   end,
+  vim.api.nvim_set_keymap("n", "<leader>gm", ":GitMessenger<CR>", { noremap = true, silent = true }),
 }
