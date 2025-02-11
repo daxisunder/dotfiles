e8fef87362213641c19d5186f39dc926a17f6007
daxis <no-prophet@proton.me>
2025-02-11 17:48:46 +0100

    remove vim-floaterm keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Floaterm
-vim.api.nvim_set_keymap("n", "<F7>", ":FloatermNew<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<F8>", ":FloatermKill<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<F9>", ":FloatermToggle<CR>", { noremap = true })
