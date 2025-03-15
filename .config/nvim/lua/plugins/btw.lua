eeccde731a5579506c69100baab028b2b08d54fd
daxis <no-prophet@proton.me>
2025-03-16 00:46:37 +0100

    remove unused plugins

--- .config/nvim/lua/plugins/btw.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "letieu/btw.nvim",
-  enabled = false,
-  config = function()
-    require("btw").setup({
-      text = "I use Arch, btw... and NeoVim too.",
-    })
-  end,
-}
--- .config/nvim/lua/plugins/telescope-frecency.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-telescope/telescope-frecency.nvim",
-  -- install the latest stable version
-  version = "*",
-  config = function()
-    require("telescope").load_extension("frecency")
-  end,
-}
