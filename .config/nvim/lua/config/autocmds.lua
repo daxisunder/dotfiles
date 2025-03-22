3b3bfeef0980c218796b786c76444c7a1810dd3c
daxis <no-prophet@proton.me>
2025-03-22 21:35:31 +0100

    mini.files border = rounded

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -68,6 +68,20 @@ vim.api.nvim_create_autocmd("User", {
   end,
 })
 
+-- Configure mini.files (window border)
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesWindowOpen",
+  callback = function(args)
+    local win_id = args.data.win_id
+
+    -- Customize window-local settings
+    vim.wo[win_id].winblend = 20
+    local config = vim.api.nvim_win_get_config(win_id)
+    config.border = "rounded"
+    vim.api.nvim_win_set_config(win_id, config)
+  end,
+})
+
 -- Disable auto-commenting new lines
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
