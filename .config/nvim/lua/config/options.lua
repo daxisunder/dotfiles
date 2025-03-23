d1962d4fad984f3f463bc394fc621b1867988b77
daxis <no-prophet@proton.me>
2025-03-23 09:52:03 +0100

    Add files via upload

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,18 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local g = vim.g
 local opt = vim.opt
 
-opt.startofline = true
+g.loaded_perl_provider = 0
 opt.cursorcolumn = true
-opt.scrolloff = 8
-opt.sidescrolloff = 8
 opt.list = true
 opt.listchars = {
+  conceal = "󰇙",
+  extends = "󰇘",
+  nbsp = "󱥸",
+  precedes = "󰇘",
   tab = "󱗽·",
   trail = "·",
-  extends = "󰇘",
-  precedes = "󰇘",
-  conceal = "󰇙",
-  nbsp = "󱥸",
 }
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.startofline = true
