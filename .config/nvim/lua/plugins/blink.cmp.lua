39a851f1355ea291685a575b16af913db99b559b
daxisunder <no-prophet@proton.me>
2025-04-27 19:36:11 +0200

    add blink keys

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -15,6 +15,25 @@ return {
   lazy = true,
   event = "InsertEnter",
   opts = {
+    keymap = {
+      preset = "none",
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide" },
+      ["<C-y>"] = { "select_and_accept" },
+
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+      ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+    },
     completion = {
       menu = {
         auto_show = true,
@@ -47,10 +66,10 @@ return {
     signature = {
       enabled = true,
       trigger = {
-        enabled = false,
-        show_on_keyword = true,
+        enabled = true,
+        show_on_keyword = false,
         show_on_trigger_character = true,
-        show_on_insert = true,
+        show_on_insert = false,
         show_on_insert_on_trigger_character = true,
       },
       window = {
