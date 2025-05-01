b45ac4f0f060d32452be88ef4ff04b4b8e11b127
daxisunder <no-prophet@proton.me>
2025-05-01 18:22:38 +0200

    remove symbols-usage, add dashboard to emacs

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -5,9 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(@ all-the-icons-completion all-the-icons-dired all-the-icons-nerd-fonts direnv
-     docker editorconfig-generate mbsync shfmt treemacs-all-the-icons
-     wakatime-mode)))
+   '(@ all-the-icons-completion all-the-icons-dired all-the-icons-nerd-fonts
+     dashboard dashboard-project-status direnv docker editorconfig-generate
+     mbsync shfmt treemacs-all-the-icons wakatime-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
