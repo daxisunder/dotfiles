75126cb0e9738d50eb5b8b9bcbbab3c54fabdfac
daxisunder <no-prophet@proton.me>
2025-05-08 13:40:05 +0200

    activate sear-cursor again

--- .config/nvim/lua/plugins/smear-cursor.lua
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -1,23 +1,19 @@
 return {
   "sphamba/smear-cursor.nvim",
-  enabled = false,
-  event = "VeryLazy",
+  enabled = true,
+  event = "CursorMoved",
   cond = vim.g.neovide == nil,
   opts = {
     hide_target_hack = true,
     -- Smear cursor color. Defaults to Cursor GUI color if not set.
     -- Set to "none" to match the text color at the target cursor position.
-    cursor_color = "#c0caf5",
-
+    cursor_color = "none",
     -- Background color. Defaults to Normal GUI background color if not set.
-    normal_bg = "#000000",
-
+    normal_bg = "none",
     -- Smear cursor when switching buffers or windows.
     smear_between_buffers = true,
-
     -- Smear cursor when moving within line or to neighbor lines.
     smear_between_neighbor_lines = true,
-
     -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
     -- Smears will blend better on all backgrounds.
     legacy_computing_symbols_support = true,
