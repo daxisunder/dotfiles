6d078c0157a9eeaa6c066051326786907e3a736c
daxisunder <no-prophet@proton.me>
2025-05-11 01:24:50 +0200

    add lazydev to lspconfig
    needs testing

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -10,6 +10,17 @@ return {
     "saghen/blink.cmp",
     "mason.nvim",
     { "williamboman/mason-lspconfig.nvim", config = function() end },
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
   },
   event = "LazyFile",
   opts = {
