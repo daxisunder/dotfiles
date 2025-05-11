325bc1c2f08d4713c7d11eb929b55f9e2491a395
daxisunder <no-prophet@proton.me>
2025-05-11 04:07:12 +0200

    fix ghost_text in blink

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -122,11 +122,11 @@ return {
         },
       },
       ghost_text = {
-        enabled = false,
-        show_with_menu = true,
-        show_without_menu = false,
-        show_with_selection = true,
-        show_without_selection = false,
+        enabled = true,
+        show_with_menu = false,
+        show_without_menu = true,
+        show_with_selection = false,
+        show_without_selection = true,
       },
       keyword = {
         range = "full",
