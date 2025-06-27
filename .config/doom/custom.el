779a0c9caf0ea596105475133cc1c19ad6b03602
daxisunder <no-prophet@proton.me>
2025-06-27 06:59:32 +0200

    update doom

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -5,18 +5,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(@ all-ext all-the-icons-completion all-the-icons-dired all-the-icons-gnus
-     all-the-icons-ibuffer all-the-icons-ivy all-the-icons-ivy-rich
-     all-the-icons-nerd-fonts compile-multi-all-the-icons dashboard
-     dashboard-project-status direnv docker editorconfig-generate evil-org
-     lsp-mode lsp-ui mbsync org-ac org-ai org-beautify-theme org-roam
-     org-roam-ui org-upcoming-modeline selected-window-accent-mode shfmt
-     spaceline-all-the-icons treemacs-all-the-icons wakatime-mode)))
+   '(all-the-icons-completion all-the-icons-nerd-fonts dashboard-project-status
+     org-beautify-theme org-upcoming-modeline smart-mode-line-powerline-theme
+     treemacs-all-the-icons wakatime-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(doom-dashboard-banner ((t (:inherit font-lock-comment-face :foreground "#7dcfff"))))
- '(mode-line ((t (:background "black" :foreground "#ffffff")))))
+ '(mode-line ((t (:background "#1a1b26" :foreground "#ffffff"))))
+ '(vertical-border ((t (:background "#1a1b26" :foreground "#1a1b26"))))
+ '(window-divider ((t (:background "#1a1b26" :foreground "#1a1b26")))))
 (put 'customize-face 'disabled nil)
