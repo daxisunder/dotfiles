ea8cc78333761d9d038f44154d34b533f4f7e0bf
daxisunder <no-prophet@proton.me>
2025-06-28 09:59:17 +0200

    add keys

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -76,6 +76,16 @@ return {
           cmp.accept({ index = 8 })
         end,
       },
+      ["<A-9>"] = {
+        function(cmp)
+          cmp.accept({ index = 9 })
+        end,
+      },
+      ["<A-0>"] = {
+        function(cmp)
+          cmp.accept({ index = 0 })
+        end,
+      },
     },
     completion = {
       menu = {
