dd501a686f231e5a679cb2d0a337d0dc6460eea1
daxisunder <no-prophet@proton.me>
2025-07-07 03:43:51 +0200

    config githead

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -5,7 +5,50 @@ return {
 		type = ui.Border.ROUNDED,
 	}),
 	require("yaziline"):setup(),
-	require("githead"):setup(),
+	require("githead"):setup({
+		order = {
+			"__spacer__",
+			"branch",
+			"commit",
+			"__spacer__",
+			"behind_ahead_remote",
+			"__spacer__",
+			"untracked",
+			"state",
+			"unstaged",
+			"__spacer__",
+			"staged",
+		},
+
+		show_numbers = true,
+
+		show_branch = true,
+		branch_prefix = "",
+		branch_color = "#288BD2",
+
+		always_show_commit = true,
+		commit_color = "#859A00",
+
+		show_behind_ahead_remote = true,
+		behind_remote_symbol = "↓",
+		ahead_remote_symbol = "↑",
+		behind_remote_color = "#DC322E",
+		ahead_remote_color = "#4DB6AC",
+
+		show_state = true,
+		show_state_prefix = false,
+		state_symbol = "!!",
+		state_color = "#B58901",
+
+		staged_symbol = "✔",
+		staged_color = "green",
+
+		unstaged_symbol = "Δ",
+		unstaged_color = "#288BD2",
+
+		untracked_symbol = "?",
+		untracked_color = "#415F65",
+	}),
 	require("git"):setup(),
 	require("restore"):setup({
 		position = { "center", w = 70, h = 40 },
