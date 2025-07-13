87c92608e1aa9b29c1a5d8c403f69137e1ad239a
daxisunder <no-prophet@proton.me>
2025-07-13 15:28:23 +0200

    override files on paste

--- .config/yazi/plugins/smart-paste.yazi/main.lua
+++ .config/yazi/plugins/smart-paste.yazi/main.lua
@@ -1,13 +1,14 @@
+--- @diagnostic disable: undefined-global
 --- @sync entry
 return {
 	entry = function()
 		local h = cx.active.current.hovered
 		if h and h.cha.is_dir then
 			ya.mgr_emit("enter", {})
-			ya.mgr_emit("paste", {})
+			ya.mgr_emit("paste", { force = true })
 			ya.mgr_emit("leave", {})
 		else
-			ya.mgr_emit("paste", {})
+			ya.mgr_emit("paste", { force = true })
 		end
 	end,
 }
