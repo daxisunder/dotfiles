85b4ab31633d545ca498c138fe8d6d972f0b7a57
daxisunder <no-prophet@proton.me>
2025-07-17 18:34:21 +0200

    adjust keymaps

--- .config/hypr/scripts/KeyHints.sh
+++ .config/hypr/scripts/KeyHints.sh
@@ -21,7 +21,7 @@ height=$((y_mon * hypr_scale / 100))
 
 # Set maximum width and height
 max_width=1200
-max_height=1000
+max_height=990
 
 # Set percentage of screen size for dynamic adjustment
 percentage_width=70
@@ -38,17 +38,17 @@ dynamic_height=$(($dynamic_height > $max_height ? $max_height : $dynamic_height)
 # Launch yad with calculated width and height
 GDK_BACKEND=$BACKEND yad --width=$dynamic_width --height=$dynamic_height \
   --center \
-  --title="Keybindings" \
+  --title="Cheat Sheet" \
   --no-buttons \
   --list \
   --column=Key: \
   --column=Description: \
   --column=Command: \
   --timeout-indicator=bottom \
-  "ESC" "close this app" "" "=" "SUPER KEY (Windows Key)" "(SUPER KEY)" \
+  "Esc" "Close this window" "" \
   " enter" "Terminal" "(kitty)" \
   " SHIFT enter" "DropDown Terminal" "(kitty-pyprland)" \
-  " SHIFT B" "Searchable Keybinds" "(Keybinds)" \
+  " ALT B" "Searchable Keybinds" "(Keybinds)" \
   " A" "Desktop Overview" "(AGS Overview)" \
   " R" "App Launcher" "(rofi-wayland)" \
   " T" "Open File Manager" "(Thunar)" \
@@ -60,14 +60,12 @@ GDK_BACKEND=$BACKEND yad --width=$dynamic_width --height=$dynamic_height \
   " W" "Choose wallpaper" "(Wallpaper Menu)" \
   " Shift W" "Choose wallpaper effects" "(imagemagick + swww)" \
   "CTRL ALT W" "Random wallpaper" "(via swww)" \
-  " B" "Hide/UnHide Waybar" "waybar" \
-  " CTRL B" "Choose waybar styles" "(waybar styles)" \
-  " ALT B" "Choose waybar layout" "(waybar layout)" \
+  " D" "Hide/UnHide Waybar" "waybar" \
   " ALT R" "Reload Waybar swaync Rofi" "CHECK NOTIFICATION FIRST!!!" \
-  " SHIFT N" "Launch Notification Panel" "swaync Notification Center" \
+  " ALT N" "Launch Notification Panel" "swaync Notification Center" \
   " Print" "screenshot" "(grim)" \
   " Shift Print" "screenshot region" "(grim + slurp)" \
-  " Shift S" "screenshot region" "(swappy)" \
+  " S" "screenshot region" "(swappy)" \
   " CTRL Print" "screenshot timer 5 secs " "(grim)" \
   " CTRL SHIFT Print" "screenshot timer 10 secs " "(grim)" \
   "ALT Print" "Screenshot active window" "active window only" \
@@ -78,10 +76,9 @@ GDK_BACKEND=$BACKEND yad --width=$dynamic_width --height=$dynamic_height \
   " ALT L" "Toggle Dwindle | Master Layout" "Hyprland Layout" \
   " Shift F" "Toggle float" "single window" \
   " ALT F" "Toggle all windows to float" "all windows" \
-  " SHIFT G" "Gamemode! All animations OFF or ON" "toggle" \
+  " ALT G" "Gamemode! All animations OFF or ON" "toggle" \
   " ALT E" "Rofi Emoticons" "Emoticon" \
   " ALT V" "Clipboard Manager" "cliphist" \
   " H" "Launch this app" "" \
   " E" "View or EDIT Keybinds, Settings, Monitor" "" \
-  "" "" "" \
-  "More tips:" "https://github.com/JaKooLit/Hyprland-Dots/wiki" ""
+  "" "" ""
