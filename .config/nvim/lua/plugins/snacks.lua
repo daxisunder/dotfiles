a8365e0ce928549bfb7ee7b123819621414bec09
daxisunder <no-prophet@proton.me>
2025-07-23 02:19:29 +0200

    add 'scope' maps

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -343,12 +343,37 @@ return {
     scope = {
       enabled = true,
       keys = {
+        textobject = {
+          ii = {
+            min_size = 2, -- minimum size of the scope
+            edge = false, -- inner scope
+            cursor = false,
+            treesitter = { blocks = { enabled = false } },
+            desc = "inner scope",
+          },
+          ai = {
+            cursor = false,
+            min_size = 2, -- minimum size of the scope
+            treesitter = { blocks = { enabled = false } },
+            desc = "full scope",
+          },
+        },
         jump = {
           ["[i"] = {
-            min_size = 2,
+            min_size = 1, -- allow single line scopes
+            bottom = false,
+            cursor = false,
+            edge = true,
+            treesitter = { blocks = { enabled = false } },
+            desc = "jump to top edge of scope",
           },
           ["]i"] = {
-            min_size = 2,
+            min_size = 1, -- allow single line scopes
+            bottom = true,
+            cursor = false,
+            edge = true,
+            treesitter = { blocks = { enabled = false } },
+            desc = "jump to bottom edge of scope",
           },
         },
       },
