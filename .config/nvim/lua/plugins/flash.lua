1a1b88588c2da9812e85774d32e893c6b2e05069
daxisunder <no-prophet@proton.me>
2025-07-29 23:36:46 +0200

    add flash keymaps

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -37,6 +37,56 @@ return {
           },
         },
       },
+      keys = {
+        {
+          "s",
+          mode = { "n", "x", "o" },
+          function()
+            require("flash").jump()
+          end,
+          desc = "Flash",
+        },
+        {
+          "S",
+          mode = { "n", "o", "x" },
+          function()
+            require("flash").treesitter()
+          end,
+          desc = "Flash Treesitter",
+        },
+        {
+          "<leader>cw",
+          mode = "n",
+          function()
+            require("flash").jump({ pattern = vim.fn.expand("<cword>") })
+          end,
+          desc = "Flash Current Word",
+        },
+        {
+          "r",
+          mode = "o",
+          function()
+            require("flash").remote()
+          end,
+          desc = "Remote Flash",
+        },
+        {
+          "R",
+          mode = { "o", "x" },
+          function()
+            require("flash").treesitter_search()
+          end,
+          desc = "Treesitter Search",
+        },
+        {
+          "<c-s>",
+          mode = { "c" },
+          function()
+            require("flash").toggle()
+          end,
+          desc = "Toggle Flash Search",
+        },
+      },
     },
   },
 }
