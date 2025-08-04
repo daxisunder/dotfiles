b768e8a86bcd0fb83e8e5b926d5c082b2d42a237
daxisunder <no-prophet@proton.me>
2025-08-05 00:45:14 +0200

    treesiter config from lazyvvim docs

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  version = false, -- last release is way too old and doesn't work on Windows
+  build = ":TSUpdate",
+  event = { "LazyFile", "VeryLazy" },
+  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+  init = function(plugin)
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require("nvim-treesitter.query_predicates")
+  end,
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   opts = function(_, opts)
     vim.list_extend(opts.ensure_installed, {
       "bash",
@@ -32,11 +41,11 @@ return {
       "yaml",
     })
   end,
-  sync_install = true,
-  auto_install = false,
   vim.filetype.add({
     pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
   }),
+  sync_install = false,
+  auto_install = true,
   highlight = {
     enable = true,
   },
@@ -46,4 +55,10 @@ return {
   indent = {
     enable = true,
   },
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
