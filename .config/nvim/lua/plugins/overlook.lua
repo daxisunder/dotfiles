27ecc97e5802c176c465ec63efd791b755ce7aa0
daxisunder <no-prophet@proton.me>
2025-09-16 21:30:04 +0200

    overlook keymaps

--- .config/nvim/lua/plugins/overlook.lua
+++ .config/nvim/lua/plugins/overlook.lua
@@ -10,63 +10,63 @@ return {
       function()
         require("overlook.api").peek_definition()
       end,
-      desc = "Overlook: Peek Definition",
+      desc = "Peek Definition",
     },
     {
       "<leader>Oc",
       function()
         require("overlook.api").peek_cursor()
       end,
-      desc = "Overlook: Peek Cursor",
+      desc = "Peek Cursor",
     },
     {
       "<leader>OQ",
       function()
         require("overlook.api").close_all()
       end,
-      desc = "Overlook: Close All Popups",
+      desc = "Close All Popups",
     },
     {
       "<leader>Or",
       function()
         require("overlook.api").restore_popup()
       end,
-      desc = "Overlook: Restore Popup",
+      desc = "Restore Popup",
     },
     {
       "<leader>OR",
       function()
         require("overlook.api").restore_all_popups()
       end,
-      desc = "Overlook: Restore All Popups",
+      desc = "Restore All Popups",
     },
     {
       "<leader>Os",
       function()
         require("overlook.api").open_in_split()
       end,
-      desc = "Overlook: Open Popup In Split",
+      desc = "Open Popup In Split",
     },
     {
       "<leader>Ov",
       function()
         require("overlook.api").open_in_vsplit()
       end,
-      desc = "Overlook: Open Popup In VSplit",
+      desc = "Open Popup In VSplit",
     },
     {
       "<leader>Ot",
       function()
         require("overlook.api").open_in_tab()
       end,
-      desc = "Overlook: Open Popup In New Tab",
+      desc = "Open Popup In New Tab",
     },
     {
       "<leader>Oo",
       function()
         require("overlook.api").open_in_original_window()
       end,
-      desc = "Overlook: Open Popup In Original Window",
+      desc = "Open Popup In Original Window",
     },
   },
 }
