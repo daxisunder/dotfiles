c9c9c40fda6c6744b7666ffa704f469c06bb2def
daxisunder <no-prophet@proton.me>
2025-09-19 03:28:10 +0200

    rewrito to respect new LazyVim spec

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,27 +1,31 @@
-local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   version = false, -- last release is way too old and doesn't work on Windows
-  build = ":TSUpdate",
-  event = { "LazyFile", "VeryLazy" },
-  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-  init = function(plugin)
-    require("lazy.core.loader").add_to_rtp(plugin)
-    require("nvim-treesitter.query_predicates")
-    vim.filetype.add({
-      pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-    })
+  build = function()
+    local TS = require("nvim-treesitter")
+    if not TS.get_installed then
+      LazyVim.error("Please restart Neovim and run `:TSUpdate` to use the `nvim-treesitter` **main** branch.")
+      return
+    end
+    TS.update(nil, { summary = true })
   end,
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
+  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+  event = { "LazyFile", "VeryLazy" },
+  cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
+  opts_extend = { "ensure_installed" },
+  ---@class lazyvim.TSConfig: TSConfig
+  opts = {
+    -- LazyVim config for treesitter
+    ensure_installed = {
       "bash",
+      "c",
       "css",
       "diff",
       "html",
       "hyprlang",
       "javascript",
+      "jsdoc",
       "json",
       "jsonc",
       "lua",
@@ -29,7 +33,6 @@ return {
       "luap",
       "markdown",
       "markdown_inline",
-      "php",
       "printf",
       "python",
       "query",
@@ -43,31 +46,55 @@ return {
       "typescript",
       "typst",
       "vim",
+      "vimdoc",
+      "xml",
       "yaml",
-    })
-  end,
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = { "org" },
+    },
   },
-  indent = {
-    enable = true,
-  },
+  ---@param opts lazyvim.TSConfig
   config = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+    local TS = require("nvim-treesitter")
+
+    -- some quick sanity checks
+    if not TS.get_installed then
+      return LazyVim.error("Please use `:Lazy` and update `nvim-treesitter`")
+    elseif vim.fn.executable("tree-sitter") == 0 then
+      return LazyVim.error({
+        "**treesitter-main** requires the `tree-sitter` CLI executable to be installed.",
+        "Run `:checkhealth nvim-treesitter` for more information.",
+      })
+    elseif type(opts.ensure_installed) ~= "table" then
+      return LazyVim.error("`nvim-treesitter` opts.ensure_installed must be a table")
     end
-    require("nvim-treesitter.configs").setup(opts)
-    if type(parser_configs) == "table" then
-      parser_configs.kitty = {
-        install_info = {
-          url = "https://github.com/OXY2DEV/tree-sitter-kitty",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-      }
+    -- setup treesitter
+    TS.setup(opts)
+    -- install missing parsers
+    local install = vim.tbl_filter(function(lang)
+      return not LazyVim.treesitter.have(lang)
+    end, opts.ensure_installed or {})
+    if #install > 0 then
+      TS.install(install, { summary = true }):await(function()
+        LazyVim.treesitter.get_installed(true) -- refresh the installed langs
+      end)
     end
+    -- treesitter highlighting
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(ev)
+        if LazyVim.treesitter.have(ev.match) then
+          pcall(vim.treesitter.start)
+        end
+      end,
+    })
+    -- kitty syntax highlighting
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "TSUpdate",
+      callback = function()
+        require("nvim-treesitter.parsers").kitty = {
+          install_info = {
+            url = "https://github.com/OXY2DEV/tree-sitter-kitty",
+          },
+        }
+      end,
+    })
   end,
 }
