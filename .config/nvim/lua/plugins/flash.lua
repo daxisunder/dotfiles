a805a05b123563487e7e236d29dd9c47847d92c4
daxisunder <no-prophet@proton.me>
2025-09-19 03:24:11 +0200

    ad inc-select keymap

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -86,6 +86,20 @@ return {
           end,
           desc = "Toggle Flash Search",
         },
+        -- Simulate nvim-treesitter incremental selection
+        {
+          "<c-space>",
+          mode = { "n", "o", "x" },
+          function()
+            require("flash").treesitter({
+              actions = {
+                ["<c-space>"] = "next",
+                ["<BS>"] = "prev",
+              },
+            })
+          end,
+          desc = "Treesitter Incremental Selection",
+        },
       },
     },
   },
