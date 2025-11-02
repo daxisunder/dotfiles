1a2821af83fe1266b7296065c0854a4ff0add527
daxisunder <no-prophet@proton.me>
2025-11-03 00:00:01 +0100

    Automated dotfiles update: Mon Nov  3 12:00:01 AM CET 2025

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -109,6 +109,22 @@ return {
         chart = "󰄧 ",
         image = " ",
       },
+      ---@class snacks.image.convert.Config
+      convert = {
+        notify = true, -- show a notification on error
+        ---@type snacks.image.args
+        mermaid = function()
+          local theme = vim.o.background == "light" and "neutral" or "dark"
+          return { "-i", "{src}", "-o", "{file}", "-b", "transparent", "-t", theme, "-s", "{scale}" }
+        end,
+        ---@type table<string,snacks.image.args>
+        magick = {
+          default = { "{src}[0]", "-scale", "1920x1080>" }, -- default for raster images
+          vector = { "-density", 192, "{src}[{page}]" }, -- used by vector images like svg
+          math = { "-density", 192, "{src}[{page}]", "-trim" },
+          pdf = { "-density", 192, "{src}[{page}]", "-background", "white", "-alpha", "remove", "-trim" },
+        },
+      },
     },
     indent = { enabled = false },
     input = { enabled = true },
