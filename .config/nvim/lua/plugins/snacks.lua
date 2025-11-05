574f0b3310f95032ef01fe170392e69eaa487d36
daxisunder <no-prophet@proton.me>
2025-11-06 00:00:01 +0100

    Automated dotfiles update: Thu Nov  6 12:00:01 AM CET 2025

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -541,7 +541,7 @@ return {
       function()
         Snacks.win({
           file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
+          width = 0.4,
           height = 0.6,
           wo = {
             spell = false,
