078c6384b3c423340ac15f89c923a3e537eb508c
daxisunder <no-prophet@proton.me>
2026-04-12 18:00:59 +0200

    style: update yazi and kitty configurations
    Updates to yazi and kitty configurations have been made, including changes to the flavor files and the initialization of yazi with new settings, as well as modifications to the kitty configuration to better fit the user's preferences. These updates aim to improve the overall aesthetic and usability of the configurations.

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -3,19 +3,7 @@ require("full-border"):setup({
 })
 
 require("yaziline"):setup({
-	color = "#7aa2f7",
-	secondary_color = "#3b4261",
-	default_files_color = "#3b4261", -- color of the file counter when it's inactive
-	selected_files_color = "#bb9af7",
-	yanked_files_color = "#9fe044",
-	cut_files_color = "#ff899d",
 	separator_style = "empty", -- "angly" | "curvy" | "liney" | "empty"
-	separator_open = "",
-	separator_close = "",
-	separator_open_thin = "",
-	separator_close_thin = "",
-	separator_head = "",
-	separator_tail = "",
 	select_symbol = "",
 	yank_symbol = "",
 	filename_max_length = 24, -- truncate when filename > 24
