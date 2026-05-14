1a217d0d05e6029c5f22da0caa0f904fb5fd638e
daxisunder <no-prophet@proton.me>
2026-05-14 18:00:13 +0200

    chore(config): optimize xdg-nvfilechooser loading and document keymaps
    Enable lazy loading for xdg-nvfilechooser via the VeryLazy event to improve editor startup performance and set version to false to ensure the latest plugin state is used. A comment was added to the keymap configuration to clarify that the goto_path mapping is intended for navigating to files outside the user's home directory.

--- .config/nvim/lua/plugins/xdg-nvfilechooser.lua
+++ .config/nvim/lua/plugins/xdg-nvfilechooser.lua
@@ -3,9 +3,12 @@ return {
   dependencies = {
     "folke/snacks.nvim",
   },
+  version = false,
+  event = "VeryLazy",
   config = function()
     require("xdg-nvfilechooser").setup({
       picker = "snacks",
+      -- Already set by default. Goto_path for the rare case when you want to pick a file in the outside of $HOME.
       keymaps = {
         ["<C-e>"] = { "goto_path", mode = { "n", "i" } },
       },
